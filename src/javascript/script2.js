8c3d8da780a65e784e3e9241c32ba93d3d6c86e4
Wilson Oliveira <127903528+woliveira1728@users.noreply.github.com>
2023-06-12 21:38:42 -0400

    Update script2.js

--- src/javascript/script2.js
+++ src/javascript/script2.js
@@ -3,5 +3,5 @@ const main2 = document.getElementById("main2");
 const gameButton = document.getElementById("game");
 
 gameButton.addEventListener("click", ()=>{
-    location.assign("../../game.html")
+    location.assign("../game.html")
 })
